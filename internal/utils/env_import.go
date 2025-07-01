124287cb88d09546d5b1e3a61b39613e0133f204
iwa <iwaongit@gmail.com>
2025-07-01 18:26:23 +0200

    feat: prevent empty domain import

--- internal/utils/env_import.go
+++ internal/utils/env_import.go
@@ -25,6 +25,11 @@ func importDomains(appState *state.AppState) {
 
 	appState.Domains = make(map[string]state.Domain, len(domains))
 	for _, domain := range domains {
+		if domain == "" {
+			println("[WARN] Empty domain found in the DOMAINS environment variable, skipping.")
+			continue
+		}
+
 		appState.Domains[domain] = state.Domain{
 			Name:       domain,
 			ExpiryDate: time.Unix(0, 0), // Default expiry date
