b77ed19ffd682459eae852622307c297e3d7c395
iwa <iwaongit@gmail.com>
2025-07-01 17:55:37 +0200

    feat: add basic main function

---
+++ cmd/domain-expiry-watcher/main.go
@@ -0,0 +1,9 @@
+package main
+
+import internal "github.com/iwa/domain-expiry-watcher/internal/struct"
+
+func main() {
+	println(" --- Domain Expiry Watcher ---")
+
+	appState := internal.GetInstance()
+}
