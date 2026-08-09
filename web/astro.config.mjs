1146872aeecd2153ffe3c07797c782b3005ba156
iwa <iwaongit@gmail.com>
2026-08-09 17:51:04 +0200

    web: add dev proxy

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -10,6 +10,15 @@ export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true
+        }
+      }
+    }
   }
 });
